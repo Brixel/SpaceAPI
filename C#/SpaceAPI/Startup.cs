3d473515c218200ce82268f02cec77912a6da8ba
Berend Wouters <berend.wouters@ugentec.com>
2017-02-15 22:23:23 +0100

    Moved to OWIN webhost

---
+++ C#/SpaceAPI/Startup.cs
@@ -0,0 +1,21 @@
+﻿using System.Web.Http;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(SpaceAPI.Startup))]
+
+namespace SpaceAPI
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            HttpConfiguration config = new HttpConfiguration();
+            WebApiConfig.Register(config);
+            app.UseWebApi(config);
+
+            //app.UseIdentityServerBearerTokenAuthentication(new IdentityServerBearerTokenAuthenticationOptions());
+            // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
+        }
+    }
+}
