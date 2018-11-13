cf74a4b146169d4e1a45e49320be589a9fb0de21
Berend Wouters <berend.wouters@gmail.com>
2018-11-13 22:13:08 +0100

    Adds JWT authentication

--- C#/SpaceAPI/Startup.cs
+++ C#/SpaceAPI/Startup.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.IdentityModel.Tokens;
 using System.Threading.Tasks;
 using System.Web.Http;
 using IdentityServer3.AccessTokenValidation;
 using Microsoft.Owin;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Jwt;
 using Owin;
 
 [assembly: OwinStartup(typeof(SpaceAPI.Startup))]
@@ -20,7 +23,17 @@ namespace SpaceAPI
             app.UseIdentityServerBearerTokenAuthentication(new IdentityServerBearerTokenAuthenticationOptions()
             {
                 Authority = ConfigurationManager.AppSettings.Get("IdentityServiceURL"),
-                RequiredScopes = new List<string>() { ConfigurationManager.AppSettings["SpaceAPIScope"] }
+                RequiredScopes = new List<string>() { ConfigurationManager.AppSettings["SpaceAPIScope"] },
+                
+            });
+            app.UseJwtBearerAuthentication(new JwtBearerAuthenticationOptions()
+            {
+                AuthenticationMode = AuthenticationMode.Active,
+                TokenValidationParameters = new TokenValidationParameters()
+                {
+                    ValidAudience = "https://brixelidentity.azurewebsites.net/resources",
+                    ValidIssuer = "https://brixelidentity.azurewebsites.net",
+                }
             });
             app.UseWebApi(config);
 
