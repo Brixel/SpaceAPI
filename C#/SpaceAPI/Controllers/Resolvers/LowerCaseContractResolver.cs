9081f8b8b57b9661a7ada465fbeaa39d7b3b8226
Berend Wouters <berend.wouters@gmail.com>
2019-04-09 20:03:51 +0200

    Adds lowercontractresolver for spaceAPI

---
+++ C#/SpaceAPI/Controllers/Resolvers/LowerCaseContractResolver.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json.Serialization;
+
+namespace SpaceAPI.Controllers.Resolvers
+{
+    public class LowerCaseContractResolver : DefaultContractResolver
+    {
+        protected override string ResolvePropertyName(string key)
+        {
+            return key.ToLower();
+        }
+    }
+}
--- C#/SpaceAPI/Startup.cs
+++ C#/SpaceAPI/Startup.cs
@@ -8,7 +8,10 @@ using IdentityServer3.AccessTokenValidation;
 using Microsoft.Owin;
 using Microsoft.Owin.Security;
 using Microsoft.Owin.Security.Jwt;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
 using Owin;
+using SpaceAPI.Controllers.Resolvers;
 
 [assembly: OwinStartup(typeof(SpaceAPI.Startup))]
 
@@ -19,10 +22,15 @@ namespace SpaceAPI
         public void Configuration(IAppBuilder app)
         {
             var config = new HttpConfiguration();
+
+            config.Formatters.JsonFormatter.SerializerSettings = new JsonSerializerSettings()
+            {
+                ContractResolver = new LowerCaseContractResolver()
+            };
             WebApiConfig.Register(config);
             app.UseIdentityServerBearerTokenAuthentication(new IdentityServerBearerTokenAuthenticationOptions()
             {
-                AuthenticationType =  "Bearer",
+                AuthenticationType = "Bearer",
                 Authority = ConfigurationManager.AppSettings.Get("IdentityServiceURL"),
                 RequiredScopes = new List<string>() { ConfigurationManager.AppSettings["SpaceAPIScope"] },
                 ValidationMode = ValidationMode.Local
