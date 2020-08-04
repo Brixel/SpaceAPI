7b311909fd50b4a4912a0d35db5d02ac2eac75cc
Berend Wouters <berend.wouters@gmail.com>
2020-08-04 20:20:40 +0200

    Enable unit tests

--- SpaceAPI.Test/Controllers/StatusControllerTest.cs
+++ SpaceAPI.Test/Controllers/StatusControllerTest.cs
@@ -1,5 +1,7 @@
 ﻿using System.Threading.Tasks;
 using FluentAssertions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Moq;
 using NUnit.Framework;
 using SpaceAPI.Data.Contexts;
@@ -11,34 +13,42 @@ namespace SpaceAPI.Test.Controllers
     [TestFixture]
     public class StatusControllerTest
     {
-        [Test, Ignore("BECAUSE I CAN")]
+        private Mock<IHttpContextAccessor> _httpContextAccessor;
+        private LogContext _context;
+
+        [OneTimeSetUp]
+        public void TestSetup()
+        {
+            _httpContextAccessor = new Mock<IHttpContextAccessor>();
+            var dbContextOptions = new DbContextOptionsBuilder<LogContext>().UseInMemoryDatabase("LogContext").Options;
+            _context = new LogContext(_httpContextAccessor.Object, dbContextOptions);
+        }
+
+        [Test]
         public async Task OpenTest()
         {
             // Arrange
-            StatusController controller = new StatusController(It.IsAny<LogContext>());
+            var sut = new StatusController(_context);
 
             // Act
-            var response = await controller.Open();
+            var response = await sut.Open();
             // Assert
             response.Should().NotBeNull();
             response.State.Open.Should().BeTrue();
         }
 
-        [Test, Ignore("BECAUSE I CAN")]
+        [Test]
         public async Task CloseTest()
         {
             // Arrange
-
-            Mock<LogContext> contextMock = new Mock<LogContext>();
-            contextMock.Setup(x => x.StateLogs.Add(It.IsAny<StateLog>()));
+            _context.StateLogs.Add(new StateLog());
             // Act
-            StatusController controller = new StatusController(contextMock.Object);
+            StatusController controller = new StatusController(_context);
             var response = await  controller.Close();;
 
             // Assert
             response.Should().NotBeNull();
             response.State.Open.Should().BeFalse();
         }
-
     }
 }
