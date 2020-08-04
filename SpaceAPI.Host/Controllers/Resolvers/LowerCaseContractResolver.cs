ae1ec28c153299f86a7f0a2d022e96dbaa3e14f5
Berend Wouters <berend.wouters@gmail.com>
2020-08-04 19:36:14 +0200

    Do not use full .sln build

---
+++ SpaceAPI.Host/Controllers/Resolvers/LowerCaseContractResolver.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json.Serialization;
+
+namespace SpaceAPI.Host.Controllers.Resolvers
+{
+    public class LowerCaseContractResolver : DefaultContractResolver
+    {
+        protected override string ResolvePropertyName(string key)
+        {
+            return key.ToLower();
+        }
+    }
+}
